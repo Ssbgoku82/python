0e23edcffb39ab782c3b84d52e97b49bb492ef48
Ssbgoku82 <159487842+Ssbgoku82@users.noreply.github.com>
2024-11-25 21:52:48 -0600

    Class added Class variables added Still have some errors to work on. Corrected errors

--- television.py
+++ television.py
@@ -5,19 +5,11 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self, status, muted, volume, channel):
+    def __init__(self) -> None:
         """
-        Television variables
-        :param status: Television ON or OFF
-        :param muted: Television volume ON or OFF
-        :param volume: Television volume level MIN or MAX
-        :param channel: Television channel MIN or MAX
-        """
-        self.status = status
-        self.muted = muted
-        self.volume = volume
-        self.channel = channel
+        Initialize the Television status
 
+        """
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
@@ -26,7 +18,7 @@ class Television:
 
 #All the instance variables should be private.
 
-    def power(self):
+    def power(self) -> None:
         """
         To determine Television is ON or OFF
         :return: True or False
@@ -37,19 +29,19 @@ class Television:
             self.__status = True
 
 
-    def mute(self):
+    def mute(self) -> None:
         """
         To determine Television Volume is Muted or not Muted
         :return: True or False
         """
-        if self.__status:
+        if not self.__status:
+            return
+        if self.__muted:
             self.__muted = False
-            self.__volume = Television.MIN_VOLUME
         else:
             self.__muted = True
-            self.__volume = Television.MAX_VOLUME
 
-    def channel_up(self):
+    def channel_up(self) ->None:
         """
         To determine if the channel needs to increase
         :return: Minimum channel
@@ -57,7 +49,7 @@ class Television:
         if self.__status:
             self.__channel = (self.__channel + 1) % (Television.MAX_CHANNEL + 1)
 
-    def channel_down(self):
+    def channel_down(self) ->None:
         """
         This should be used to decrease the tv channel value
         :return: maximum channel
@@ -65,39 +57,36 @@ class Television:
         if self.__status:
             self.__channel = (self.__channel - 1) % (Television.MAX_CHANNEL + 1)
 
-# TODO: Still not getting the right volume in the Main.py program
-# TODO: Will continue to troubleshoot the on going failure.
 
-    def volume_up(self):
+
+    def volume_up(self) ->None:
         """
         This should be used to increase the tv volume value
         :return: Max volume
         """
         if self.__status:
+            if self.__muted:
+                self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
-                return
-            else:
-                return Television.MAX_VOLUME
 
-    def volume_down(self):
+    def volume_down(self) ->None:
         """
         This should be used to decrease the tv volume value
         :return: MIN Volume
         """
         if self.__status:
+            if self.__muted:
+                self.__muted = False
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
-                return
-            else:
-                return Television.MIN_VOLUME
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         This should send the values to the main.py
         :return: values
         """
-
+        volume_display = "On Mute" if self.__muted else self.__volume
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
 
 
